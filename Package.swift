2927211f14389604f0ea6d22991943d6ae46eb74
Geert Berkers <geertberkers@hotmail.com>
2021-06-03 10:55:10 +0200

    Added .macOS(.v10_15) platform

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 /**
  * Copyright IBM Corporation and the Kitura project authors 2018-2020
@@ -20,6 +20,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftJWT",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
